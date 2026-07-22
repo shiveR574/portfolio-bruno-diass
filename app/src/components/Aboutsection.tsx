53e180af44fc6aba48b23a68f24490b499aee7de
Bruno Dias <bruno.dias1893@gmail.com>
2026-07-22 18:53:01 +0100

    1

--- app/src/components/Aboutsection.tsx
+++ app/src/components/Aboutsection.tsx
@@ -3,6 +3,12 @@ import { Laugh } from "lucide-react";
 import Image from "next/image";
 import ranimage from "@/app/src/assets/handcoding.png";
 import ranimage2 from "@/app/src/assets/programming.png";
+import { GiBrain } from "react-icons/gi";
+import { FaHandshake } from "react-icons/fa";
+import { SiFuturelearn } from "react-icons/si";
+import { MdFactCheck } from "react-icons/md";
+import { FaPuzzlePiece } from "react-icons/fa6";
+
 
 
 export default function Aboutsection() {
@@ -14,12 +20,12 @@ export default function Aboutsection() {
                 <p className={styles["aboutsection-description"]}>Hi, I'm Bruno! I'm a passionate developer with experience in creating engaging web applications.</p>
                     <div className={styles["aboutsection-skills"]}>
                         <ul className={styles["skills-list"]}>
-                            <li className={styles["skill-item"]} ><Laugh size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} />Commitment</li>
+                            <li className={styles["skill-item"]} ><MdFactCheck size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} />Commitment</li>
                             <li className={styles["skill-item"]} ><Laugh size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} /> Communication</li>
-                            <li className={styles["skill-item"]} ><Laugh size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} /> Team Collaboration</li>
-                            <li className={styles["skill-item"]} ><Laugh size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} />Adaptability</li>
-                            <li className={styles["skill-item"]} ><Laugh size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} />Problem Solving</li>
-                            <li className={styles["skill-item"]} ><Laugh size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} />Always Learning</li>
+                            <li className={styles["skill-item"]} ><FaHandshake size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} /> Team Collaboration</li>
+                            <li className={styles["skill-item"]} ><FaPuzzlePiece size={30} style={{ marginRight: "15px", marginLeft: "2px", display: "flex", alignItems: "center", color: "#7a94dd" }} />Adaptability</li>
+                            <li className={styles["skill-item"]} ><GiBrain size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} />Problem Solving</li>
+                            <li className={styles["skill-item"]} ><SiFuturelearn size={30} style={{ marginRight: "15px", display: "flex", alignItems: "center", color: "#7a94dd" }} />Always Learning</li>
                         </ul>
                     </div>
             </div>
