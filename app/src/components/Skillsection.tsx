3088de9fd5a51c7152421a8fbeb487801f2e9c45
Bruno Dias <bruno.dias1893@gmail.com>
2026-07-23 02:09:55 +0100

    1

--- app/src/components/Skillsection.tsx
+++ app/src/components/Skillsection.tsx
@@ -1,6 +1,6 @@
 import styles from "./Skillsection.module.scss";
 import { FaReact } from "react-icons/fa";
-import { SiNextdotjs, SiTypescript, SiJavascript, SiHtml5, SiSass, SiTailwindcss, SiBootstrap } from "react-icons/si";
+import { SiNextdotjs, SiTypescript, SiJavascript, SiHtml5, SiTailwindcss, SiBootstrap } from "react-icons/si";
 
 
 export default function Skillsection() {
@@ -14,7 +14,6 @@ export default function Skillsection() {
                     <li className={styles["skill-item"]}><SiTypescript size={40} style={{  display: "flex", alignItems: "center", color: "#7a94dd" }} />TypeScript</li>
                     <li className={styles["skill-item"]}><SiJavascript size={40} style={{  display: "flex", alignItems: "center", justifySelf:"center", color: "#7a94dd" }} />JavaScript</li>
                     <li className={styles["skill-item"]}><SiHtml5 size={40} style={{  display: "flex", alignItems: "center", color: "#7a94dd" }} />HTML5</li>
-                    <li className={styles["skill-item"]}><SiSass size={40} style={{ display: "flex", alignItems: "center", color: "#7a94dd" }} />Sass</li>
                     <li className={styles["skill-item"]}><SiTailwindcss size={40} style={{ display: "flex", alignItems: "center", color: "#7a94dd" }} />Tailwind CSS</li>
                     <li className={styles["skill-item"]}><SiBootstrap size={40} style={{ display: "flex", alignItems: "center", color: "#7a94dd" }} />Bootstrap</li>
                 </ul>
