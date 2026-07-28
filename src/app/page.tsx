1a706b560d23627beac26a7aa7e76d07546c4769
Bruno Dias <bruno.dias1893@gmail.com>
2026-07-28 16:17:10 +0100

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Herosection from "../components/Herosection";
 import Aboutsection from "../components/Aboutsection";
 import Skillsection from "../components/Skillsection";
 import Skillsectiontailwind from "../components/Skillsectiontailwind";
-import Skillsectiontailwind2 from "../components/Skillsectiontailwind2";
 import Projectsection from "../components/Projectsection";
 import Footer from "../components/Footer";
 import "./page.scss";
@@ -17,7 +16,6 @@ export default function Home() {
       <Aboutsection />
       <Skillsection />
       <Skillsectiontailwind/>
-      <Skillsectiontailwind2/>
       <Projectsection />
       <Footer />
     </>
