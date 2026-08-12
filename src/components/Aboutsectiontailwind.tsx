410b4ecd6455eceed7620c0196aa208022727526
Bruno Dias <bruno.dias1893@gmail.com>
2026-08-12 21:03:20 +0100

    1

--- src/components/Aboutsectiontailwind.tsx
+++ src/components/Aboutsectiontailwind.tsx
@@ -15,7 +15,7 @@ export default function Aboutsectiontailwind() {
             <Image src={ranimage} className="w-30 h-40 md:w-100 md:h-90 object-contain" alt="Logo"></Image>
             <div className="flex flex-col ml-0 mt-[20px] md:ml-[180px] md:mt-0 justify-center md:justify-start items-center md:items-start">
                 <h2 className="text-sm md:text-4xl font-bold text-[#f7f7f7] mt-[10px]">About Me</h2>
-                <p className="text-sm md:text-2xl font-normal text-[#f7f7f7] w-auto md:max-w-[800px] mt-[15px] md:mt-[35px]">Hi, I'm Bruno! I'm a passionate developer with experience in creating engaging web applications.</p>
+                <p className="text-sm md:text-2xl font-normal text-[#f7f7f7] w-full md:max-w-[800px] mt-[15px] md:mt-[35px]">Hi, I'm Bruno! I'm a passionate developer with experience in creating engaging web applications.</p>
                     <div className="mt-[20px] md:mt-[50px]">
                         <ul className="grid grid-cols-2 gap-3 md:grid md:grid-cols-3 md:gap-5">
                             <li className="text-sm md:text-xl font-normal text-[#f7f7f7] flex flex-row items-center gap-3 md:gap-5" ><MdFactCheck className="w-5 h-5 md:w-8 md:h-8 text-[#7a94dd]"/>Commitment</li>
