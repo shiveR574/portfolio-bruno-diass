7d2db503acca26b9eb679f39dc1cf543dbc501e7
Bruno Dias <bruno.dias1893@gmail.com>
2026-08-20 19:53:32 +0100

    1

--- src/components/Navbartailwind.tsx
+++ src/components/Navbartailwind.tsx
@@ -13,12 +13,12 @@ export default function Navbartailwind() {
     <nav className="">
         <Image src={logo2} className=""alt="Logo"></Image>
         <div className="">
-            <Link href="/" className={pathname === "/" ? styles.active : undefined}>Home</Link>
-            <Link href="/about" className={pathname === "/about" ? styles.active : undefined}>About</Link>
-            <Link href="/skills" className={pathname === "/skills" ? styles.active : undefined}>Skills</Link>
-            <Link href="/projects" className={pathname === "/projects" ? styles.active : undefined}>Projects</Link>
-            <Link href="/experience" className={pathname === "/experience" ? styles.active : undefined}>Experience</Link>
-            <Link href="/contact" className={pathname === "/contact" ? styles.active : undefined}>Contact</Link>
+            <Link href="/" className={pathname === "/" ? "active" : undefined}>Home</Link>
+            <Link href="/about" className={pathname === "/about" ? "active" : undefined}>About</Link>
+            <Link href="/skills" className={pathname === "/skills" ? "active" : undefined}>Skills</Link>
+            <Link href="/projects" className={pathname === "/projects" ? "active" : undefined}>Projects</Link>
+            <Link href="/experience" className={pathname === "/experience" ? "active" : undefined}>Experience</Link>
+            <Link href="/contact" className={pathname === "/contact" ? "active" : undefined}>Contact</Link>
             <button className=""
             onClick={() => {
                 const link = document.createElement("a");
